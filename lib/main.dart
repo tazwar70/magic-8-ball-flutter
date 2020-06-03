d22d8e80cfa4739467f37fb5b26959aa1b0c474e
DESKTOP-E4UEJB8\DELL <tazwar70@gmail.com>
2020-06-03 14:09:14 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,52 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        debugShowCheckedModeBanner: false,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        title: Center(
+          child: Text(
+            'Ask me Anything',
+          ),
+        ),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballnumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {
+          setState(() {
+            ballnumber = Random().nextInt(5) + 1;
+            print(ballnumber);
+          });
+        },
+        child: Image.asset('images/ball$ballnumber.png'),
+      ),
+    );
+  }
+}
